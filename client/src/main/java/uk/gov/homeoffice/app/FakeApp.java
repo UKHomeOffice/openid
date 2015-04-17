4989481ae610769137902580b921abf21f60f3db
pyruby <chris@pyruby.co.uk>
2015-04-17 10:18:48 +0100

    Initial commit

---
+++ client/src/main/java/uk/gov/homeoffice/app/FakeApp.java
@@ -0,0 +1,7 @@
+package uk.gov.homeoffice.app;
+
+/**
+ * Created by pyruby on 17/04/15.
+ */
+public class FakeApp {
+}
---
+++ server/src/main/java/uk/gov/homeoffice/identity/OpenID.java
@@ -0,0 +1,7 @@
+package uk.gov.homeoffice.identity;
+
+/**
+ * Created by pyruby on 17/04/15.
+ */
+public class OpenID {
+}
