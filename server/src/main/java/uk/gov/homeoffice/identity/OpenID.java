62c160d4a3f513a9c5d9a1700fa5883d57323e88
pyruby <chris@pyruby.co.uk>
2015-04-17 10:25:28 +0100

    Add initial SpringBoot pieces.

--- server/src/main/java/uk/gov/homeoffice/identity/OpenID.java
+++ server/src/main/java/uk/gov/homeoffice/identity/OpenID.java
@@ -1,7 +1,18 @@
 package uk.gov.homeoffice.identity;
 
-/**
- * Created by pyruby on 17/04/15.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import java.io.IOException;
+
+@Configuration
+@EnableAutoConfiguration
+@Import({AppConfig.class})
 public class OpenID {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(OpenID.class, args);
+    }
 }
